394d1f5a86728547e536a9c7f0739f21338fff33
kevxcyj <kevcyjunior@gmail.com>
2024-05-06 10:59:59 +0000

    Added number 4

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    for attribute in names:
+        if attribute[:2] != "__":
+            print(attribute)

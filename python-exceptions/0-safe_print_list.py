5291023bdf7e6ed4255f7cbaa6b26263fe8551e1
kevxcyj <kevcyjunior@gmail.com>
2024-05-07 03:09:20 +0000

    Added number 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    list_t = 0
+    try:
+        for i in range(x):
+            list_t(my_list[i], end='')
+            list_t += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return list_t

ac4d17368247d1563b75bc356ecab0c150505b54
kevxcyj <kevcyjunior@gmail.com>
2024-05-08 15:39:24 +0000

    Removed comment

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,8 +3,7 @@ if __name__ == "__main__":
     import sys
     sum = 0
     for i in range(len(sys.argv) - 1):
-        sum += int(sys.argv[i + 1])
-        **/ Converts to integer **/ 
+        sum += int(sys.argv[i + 1]) 
     print("{}".format(sum))
 
 

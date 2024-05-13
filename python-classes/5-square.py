b345036ade728a1fece16066d5d3584652004994
kevxcyj <kevcyjunior@gmail.com>
2024-05-13 02:58:46 +0000

    Fixed whitespaces on 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -8,28 +8,26 @@ class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
-        
+
         if size < 0:
             raise ValueError('size must be >= 0')
-        
-        self.__size = size
 
+        self.__size = size
 
     @property
     def size(self):
 
         return self.__size
-    
+
     @size.setter
     def size(self, value):
        
-       if not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
 
-       if value < 0:
-           raise ValueError('size must be >= 0')
-
-       self.__size = value
+        self.__size = value
 
     def are(self):
 

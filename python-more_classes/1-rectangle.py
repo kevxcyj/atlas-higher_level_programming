14547501ab788ead85b876464e100a5a5c5e1991
kevxcyj <kevcyjunior@gmail.com>
2024-05-13 04:33:56 +0000

    Added number 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Write a class Rectangle that defines a rectangle by 0 """
+
+
+class Rectangle:
+    """ Function """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an interger")
+        else:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("height must be >= 0")
+        
+        self.__height = value

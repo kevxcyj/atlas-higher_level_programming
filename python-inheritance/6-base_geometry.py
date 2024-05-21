108223656d1d00fd3ec87ad9514a31b3cf042578
kevxcyj <kevcyjunior@gmail.com>
2024-05-21 07:03:09 +0000

    Added number 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Defines the class BaseGeometry """
+
+
+class BaseGeometry:
+
+    def area(self):
+        """ Raise exception """
+
+        raise Exception("area() is not implemented")

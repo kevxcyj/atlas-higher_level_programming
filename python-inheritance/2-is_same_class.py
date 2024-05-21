571a41fe9bc7b6b70b15575046809ba21ebe1dd2
kevxcyj <kevcyjunior@gmail.com>
2024-05-21 06:45:09 +0000

    Added number 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#1/usr/bin/python3
+
+""" Defines is_same_class """
+
+
+def is_same_class(obj, a_class):
+    """ Checks if object is instance of specified class """
+
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False

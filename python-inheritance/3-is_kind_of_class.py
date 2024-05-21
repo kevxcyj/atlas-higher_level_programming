8f72d6966dca480d4c9209a2f3275681ca2b752f
kevxcyj <kevcyjunior@gmail.com>
2024-05-21 06:50:52 +0000

    Fixed whitespace

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """ Checks if object is and instance of, or if the object 
+    """ Checks if object is and instance of, or if the object
     is an instance of a class that inherited from """
 
     return isinstance(obj, a_class)

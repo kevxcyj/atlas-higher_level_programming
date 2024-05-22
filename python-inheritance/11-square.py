0f40fc564cacf286dd6119cd137316ca3b108439
kevxcyj <kevcyjunior@gmail.com>
2024-05-22 04:51:45 +0000

    Added number 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
-def lookup(obj):
-    return dir(obj)
+""" Class Square """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Defines square based on 10-square.py """
+    def __init__(self, size):
+
+        """ Size = length """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+        super().__init__(size, size)

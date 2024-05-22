7abd7f729b27d87a69bcb6abca7cbe694a58e2f1
kevxcyj <kevcyjunior@gmail.com>
2024-05-22 04:00:22 +0000

    Added number 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines class_to_json """
+
+
+def class_to_json(obj):
+    """ Returns the dictionary description with simple
+        data structure """
+
+    return (obj.__dict__)

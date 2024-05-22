d2de2dbc588db3f6e5829a850e35b97d2960bdcb
kevxcyj <kevcyjunior@gmail.com>
2024-05-22 03:29:37 +0000

    Added numebr 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+""" Defines append_write """
+
+
+def append_write(filename="", text=""):
+    """ Appends a string at the end of a text file """
+
+    with open(filename, "a") as f:
+        return f.write(text)

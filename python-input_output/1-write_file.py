4a636eb11c942e187166eef51981664aa4594698
kevxcyj <kevcyjunior@gmail.com>
2024-05-22 03:21:43 +0000

    Added number 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines write_file """
+
+
+def write_file(filename="", text=""):
+    -""" Writes a string to a text file and returns
+         the number of characters writen """
+    
+    with open(filename, "w") as f:
+        return (f.write(text))

7c666d9cc22a23cad844630f637b9b20ec33b63c
kevxcyj <kevcyjunior@gmail.com>
2024-05-27 21:27:30 +0000

    Fixed init

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ from models.base import Base
 class Rectangle(Base):
     """ Class Rectangle """
 
-    def _init_(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """ Attributes for rectangle
             width = width of rectangle
             height = height of rectangle """

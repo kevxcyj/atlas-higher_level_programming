f6af954026234311e5d752c5fa49d0c22fbe75f3
kevxcyj <kevcyjunior@gmail.com>
2024-05-27 21:31:37 +0000

    Set properties to attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,3 +17,43 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """Function for width of rectangle"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        self.validate_integers("width", value, False)
+        self.__width = value
+
+    @property
+    def height(self):
+        """Function for height of rectangle"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        self.validate_integers("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        """Function for x of rectangle"""
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        self.validate_integers("x", value, True)
+        self.__x = value
+
+    @property
+    def y(self):
+        """Function for y of rectangle"""
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        self.validate_integers("y", value, True)
+        self.__y = value

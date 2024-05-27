22d1a470a7d0c409a32d602def2f0babc0e141b0
kevxcyj <kevcyjunior@gmail.com>
2024-05-27 23:20:31 +0000

    Added json method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,3 +16,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns string representation of list_dictioonaries """
+        if list_dictionaries is None or not list_dictionaries:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))

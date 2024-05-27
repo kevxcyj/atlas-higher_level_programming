a53cfe7edc83afe5dfe4c37b299c41afd54054aa
kevxcyj <kevcyjunior@gmail.com>
2024-05-27 21:21:28 +0000

    Created rectangle class and assigned width and height

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Inheriting from base.py """
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Class Rectangle """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Attributes for rectangle
+            width = width of rectangle
+            height = height of rectangle """
+        super().__init__(id)
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y

e44b9fe1cadb1d3e6dbb6edc5a18263164cb2338
kevxcyj <kevcyjunior@gmail.com>
2024-05-27 21:41:04 +0000

    Added if statements

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,40 +20,60 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Function for width of rectangle"""
+        """ Rectangle width """
         return (self.__width)
 
     @width.setter
     def width(self, value):
-        self.validate_integers("width", value, False)
+        
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = value
 
     @property
     def height(self):
-        """Function for height of rectangle"""
+        """ Rectangle height """
         return (self.__height)
 
     @height.setter
     def height(self, value):
-        self.validate_integers("height", value, False)
+    
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
         self.__height = value
 
     @property
     def x(self):
-        """Function for x of rectangle"""
+        """ X of Rectangle """
         return (self.__x)
 
     @x.setter
     def x(self, value):
-        self.validate_integers("x", value, True)
+        
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = value
 
     @property
     def y(self):
-        """Function for y of rectangle"""
+        """ Y of Rectangle """
         return (self.__y)
 
     @y.setter
     def y(self, value):
-        self.validate_integers("y", value, True)
+
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
         self.__y = value

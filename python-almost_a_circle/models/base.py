fd6de969268c1cfc1329f65bb025c1a756241463
kevxcyj <kevcyjunior@gmail.com>
2024-05-27 23:28:17 +0000

    Fixed space

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,7 +33,6 @@ class Base:
 
         filename = f"{cls.__name__}.json"
         with open(filename, "w") as file:
-            json_string = cls.to_json_string
-            ([obj.to_dictionary() for obj in list_objs])
+            json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
 
             file.write(json_string)

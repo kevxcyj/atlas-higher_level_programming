7d33473deb039c68046b5f82bdbcca4688ff5e06
kevxcyj <kevcyjunior@gmail.com>
2024-05-28 21:53:50 +0000

    Fixed if statement

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-""" Defines class BaseGeometry """
+""" Defines class BaseGeometry base on 6-base.geometry """
 
 
 class BaseGeometry:
     """ Raises exception """
     def area(self):
-
         raise Exception("area() is not implemented")
     
 def integer_validator(self, name, value):
@@ -15,6 +14,7 @@ def integer_validator(self, name, value):
     """ Return if not an integer """
 
     if type(value) is not int:
-        raise TypeError(f"{name} must be an integer")
+        raise TypeError("{} must be an integer".format(name))
+
     if value <= 0:
-        raise ValueError(f"{name} must be greater than 0")
+        raise ValueError("{} must be greater than 0".format(name))

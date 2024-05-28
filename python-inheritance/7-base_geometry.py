5a95bb8aefd476627e0efb0e7f34de9b8f893a16
kevxcyj <kevcyjunior@gmail.com>
2024-05-28 22:13:03 +0000

    Fixed indentation

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,13 +8,13 @@ class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
     
-def integer_validator(self, name, value):
-    """ Checks if the value is an integer is greater than zero """
+    def integer_validator(self, name, value):
+        """ Checks if the value is an integer is greater than zero """
 
-    """ Return if not an integer """
+        """ Return if not an integer """
 
-    if type(value) is not int:
-        raise TypeError("{} must be an integer".format(name))
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
 
-    if value <= 0:
-        raise ValueError("{} must be greater than 0".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

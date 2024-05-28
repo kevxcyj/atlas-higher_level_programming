98f178a56218029a22a6c7a08eb8f1eedea7e4de
kevxcyj <kevcyjunior@gmail.com>
2024-05-28 22:29:29 +0000

    fixed indentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,10 +7,10 @@ class Mylist(list):
     """ Creating the class Mylist """
 
     def print_sorted(self):
-        """ Prints integers and sorts in order """
+            """ Prints integers and sorts in order """
 
-        for i in self:
-            if not isinstance(i, int):
-                raise TypeError()
+            for i in self:
+                if not isinstance(i, int):
+                    raise TypeError()
 
-        print(sorted(self))
+            print(sorted(self))

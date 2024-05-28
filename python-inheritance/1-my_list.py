24d3ee66f24d7c0d44f242989d5c6ea9e1c3fffe
kevxcyj <kevcyjunior@gmail.com>
2024-05-28 22:44:07 +0000

    Fixed documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-""" Function that inherits for Mylist and creates a list of integers """
+""" Function that inherits for Mylist module and creates a list of integers """
 
 
 class Mylist(list):
-        """ Creating the class Mylist that inherits from list """
+    """ Creating the class Mylist that inherits from list """
 
-        def print_sorted(self):
-            """ Prints integers and sorts in order """
+    def print_sorted(self):
+        """ Prints integers and sorts in order """
 
-            print(sorted(self))
+        print(sorted(self))

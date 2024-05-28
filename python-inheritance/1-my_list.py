420e4064e7ecb7754b6a043715997b02429d6683
kevxcyj <kevcyjunior@gmail.com>
2024-05-28 22:52:37 +0000

    Import

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Function that inherits for Mylist module and creates a list of integers """
 
+MyList = __import__('1-my_list').MyList
+
 class Mylist(list):
     """ Mylist class - that inherits from list """
     def print_sorted(self):

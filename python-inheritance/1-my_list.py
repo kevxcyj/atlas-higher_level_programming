0c325a7978f369f6c57c55f978293b0d3f9f11c3
kevxcyj <kevcyjunior@gmail.com>
2024-05-28 22:51:36 +0000

    idk

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,8 +2,7 @@
 """ Function that inherits for Mylist module and creates a list of integers """
 
 class Mylist(list):
-    """ Mylist class - that inherits from list 
-        python3 -c "d = __import__(\"1-my_list\").MyList.__doc__ """
+    """ Mylist class - that inherits from list """
     def print_sorted(self):
         """ Prints integers and sorts in order """
 

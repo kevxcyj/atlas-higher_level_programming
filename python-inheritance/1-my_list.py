0b326112cc805eaa5a82866676be3746441044d0
kevxcyj <kevcyjunior@gmail.com>
2024-05-28 22:53:14 +0000

    Deleted

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Function that inherits for Mylist module and creates a list of integers """
 
-MyList = __import__('1-my_list').MyList
 
 class Mylist(list):
     """ Mylist class - that inherits from list """

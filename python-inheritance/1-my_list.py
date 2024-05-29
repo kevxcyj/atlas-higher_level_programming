5d481958337299cc503ed3e7b9686f378112fe59
kevxcyj <kevcyjunior@gmail.com>
2024-05-29 04:10:11 +0000

    Added number 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,13 @@
 
 
 class Mylist(list):
-    """ Mylist class - that inherits from list """
+    """
+    Inherits from Python's built-in list class to create a specialized list for handling integers.
+
+    Attributes:
+        __init__: Initializes the Mylist object, inheriting from the list class.
+        print_sorted: A method to sort the integers in the Mylist object and print them in ascending order.
+    """
     def print_sorted(self):
         """ Prints integers in Mylist and sorts in order """
 

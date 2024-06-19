b66361ad90066a85a83109af6b353605a9b47f8a
kevxcyj <kevcyjunior@gmail.com>
2024-06-19 04:22:02 +0000

    Fixed code in number 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,24 +7,18 @@ from sys import argv
 
 def connect_mysql():
     """Connect to MySQL server """
-    db = MySQLdb.connect(
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        host="localhost",
-        port=3306
-        )
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states \
-        WHERE name LIKE BINARY 'N%' \
-        ORDER BY states.id ASC"
-        )
+     cur = db.cursor()
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cur.execute(nmeSr)
+
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
+    for i in rows:
+        print(i)
+    cur.close()
+    db.close()
 
 if __name__ == '__main__':
     connect_mysql()

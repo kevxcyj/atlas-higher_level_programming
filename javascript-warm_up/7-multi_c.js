c87ece00d650d7cd0b04bdd23295ab88706e6ded
kevxcyj <kevcyjunior@gmail.com>
2024-07-23 03:04:54 -0500

    Semistandard

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const line = 'C is fun';
 
 if (isNaN(process.argv[2])) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < parseInt(process.argv[2]); i++) {
-      console.log(language);
-    }
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(line);
   }
+}

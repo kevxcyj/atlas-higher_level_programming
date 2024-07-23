24c6e5d01f127c2115c0cf5d4f0763b92dae4f74
kevxcyj <kevcyjunior@gmail.com>
2024-07-23 03:11:15 -0500

    Added number 1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Empty class that defines rectangle
+
+class Rectangle {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  module.exports = Rectangle;

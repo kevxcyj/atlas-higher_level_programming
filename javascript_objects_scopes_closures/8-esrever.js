ae71bb196d7492ae9bb8dfdf69d890458d9b39b6
kevxcyj <kevcyjunior@gmail.com>
2024-07-23 03:34:50 -0500

    Semistandard

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,10 +2,10 @@
 // Returns reversed version of list
 
 exports.esrever = function (list) {
-    const reversedList = [];
-    const last = list.length - 1;
-    for (let i = last; i >= 0; i--) {
-      reversedList.push(list[i]);
-    }
-    return (reversedList);
-  };
+  const reversedList = [];
+  const last = list.length - 1;
+  for (let i = last; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return (reversedList);
+};
--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -2,9 +2,9 @@
 // Prints new argument value and # of existing
 
 exports.logMe = function (item) {
-    if (typeof this.count === 'undefined') {
-     this.count = 0;
-    }
-    console.log(this.count + ': ' + item);
-    this.count++;
- };
+  if (typeof this.count === 'undefined') {
+    this.count = 0;
+  }
+  console.log(this.count + ': ' + item);
+  this.count++;
+};

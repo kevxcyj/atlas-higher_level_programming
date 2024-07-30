096702fe53e8c702383be1d19bc11fef4ee16d4b
kevxcyj <kevcyjunior@gmail.com>
2024-07-30 17:38:29 -0500

    Added number 2

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,7 @@
+// Script that updates the text color of header whwen user clicks on tag
+
+$(function () {
+    $('red_header').click(function () {
+        $('header').css('color', '#FF0000');
+    });
+});

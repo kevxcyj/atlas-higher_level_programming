e5929a335396cee45ae9c0fe7df653b9be7515de
kevxcyj <kevcyjunior@gmail.com>
2024-07-30 03:40:25 -0500

    semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 // Script that reads and prints contents of file
 
-const fs = require('fs')
+const fs = require('fs');
 
 if (process.argv.length == 2) {
-    console.error('An error has occured');
-    process.exit(1);
+  console.error('An error has occured');
+  process.exit(1);
 }
 
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(data);
 });

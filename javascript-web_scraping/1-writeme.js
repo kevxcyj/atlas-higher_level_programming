6cbd8a9181055db7cb922b724c9f270434953137
kevxcyj <kevcyjunior@gmail.com>
2024-07-30 03:51:07 -0500

    Added 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length === 3) {
+  console.error('An error has occured');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const userString = process.argv[3];
+
+fs.writeFile(filePath, userString, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});

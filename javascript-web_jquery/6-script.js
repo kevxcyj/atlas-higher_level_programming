f8e10d664c3746273af40a28547476cfa9db1b8f
kevxcyj <kevcyjunior@gmail.com>
2024-07-30 17:54:53 -0500

    Added number 6

--- javascript-web_jquery/6-script.js
+++ javascript-web_jquery/6-script.js
@@ -1,2 +1,7 @@
 // Script that updates the text of the header element to New Header!!!
 
+$(function () {
+    $('#DIV#update_header').click(function () {
+        $('header').text('New Header!!!');
+    });
+});

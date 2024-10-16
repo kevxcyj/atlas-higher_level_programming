21e2e38de93963352cd26ecf0762e013dc9d496f
kevxcyj <kevcyjunior@gmail.com>
2024-10-16 11:32:17 -0600

    added number 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Takes in the name of a state as an argument, lists all cities """
+
+
+from sys import argv
+import MySQLdb
+
+
+def connect_mysql():
+    """Connect to a MySQL server """
+    db = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        host="localhost",
+        port=3306
+    )
+    with db.cursor() as cur:
+        cur.execute(
+            "SELECT cities.id, cities.name, states.name \ FROM cities \
+            JOIN states \ ON cities.state_id = states.id \
+            WHERE states.name LIKE BINARY %(state_name)s \
+            ORDER BY cities.id ASC", {'state_name': argv[4]}
+        )
+        rows = cur.fetchall()
+        if rows is not None:
+            city_names = ", ".join([row[1] for row in rows])
+            print(city_names)
+
+
+if __name__ == "__main__":
+    connect_mysql()
